e17c27c3679e7d4d8ebf443ebf7d1029e2ed060e
YangLang116 <1004145468@qq.com>
2025-05-19 17:54:05 +0800

    更新脚本

--- clash/generator.py
+++ clash/generator.py
@@ -4,33 +4,30 @@ from protocol import vpn_ss, vpn_ssr, vpn_vmess, vpn_trojan
 
 
 def _find_configs():
-    # 来自 app.py 的 URL
-    ss_url = (
-        "https://gitlab.com/zhifan999/fq/-/wikis/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"
-    )
-    vmess_url = "https://gitlab.com/zhifan999/fq/-/wikis/v2ray%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"
-    mixin_url = "https://raw.githubusercontent.com/Pawdroid/Free-servers/refs/heads/main/README.md"
-
-    # 获取ss、ssr、vmess、trojan配置
     ss_configs = []
     ssr_configs = []
-    ss_page = fetch_url(ss_url)
+    ss_page = fetch_url(
+        "https://gitlab.com/zhifan999/fq/-/wikis/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"
+    )
     ss_configs.extend(vpn_ss.find_ss_link(ss_page, r"ss://[^\\]+"))
     ssr_configs.extend(vpn_ssr.find_ssr_link(ss_page, r"ssr://[^\\]+"))
 
     vmess_configs = []
-    vmess_page = fetch_url(vmess_url)
+    vmess_page = fetch_url(
+        "https://gitlab.com/zhifan999/fq/-/wikis/v2ray%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"
+    )
     vmess_configs.extend(vpn_vmess.find_vmess_link(vmess_page, r"vmess://[^\\]+"))
 
     trojan_configs = []
-    mixin_page = fetch_url(mixin_url)
+    mixin_page = fetch_url(
+        "https://raw.githubusercontent.com/Pawdroid/Free-servers/refs/heads/main/README.md"
+    )
     ss_configs.extend(vpn_ss.find_ss_link(mixin_page, r"^ss://[^\s\n]+"))
     ssr_configs.extend(vpn_ssr.find_ssr_link(mixin_page, r"^ssr://[^\s\n]+"))
     vmess_configs.extend(vpn_vmess.find_vmess_link(mixin_page, r"^vmess://[^\s\n]+"))
     trojan_configs.extend(
         vpn_trojan.find_trojan_link(mixin_page, r"^trojan://[^\s\n]+")
     )
-
     return (ss_configs, ssr_configs, vmess_configs, trojan_configs)
 
 
