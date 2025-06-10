0f25f9a2e9533f0fd7eff48c34dd5e869dc9763d
HomeWindows <1004145468@qq.com>
2025-06-10 17:10:49 +0800

    更新检查源

--- clash/fetcher.py
+++ clash/fetcher.py
@@ -11,10 +11,12 @@ def _find_from_github_fq(nodes):
         "https://gitlab.com/zhifan999/fq/-/wikis/v2ray%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"
     )
     nodes.extend(vpn_vmess.find_vmess_link(content, r"vmess://[^\\]+"))
+    print(f"find from github fq: ${len(nodes)}")
 
 
-def _find_from_github_md(nodes):
+def _find_from_github_text(nodes):
     urls = [
+        "https://raw.githubusercontent.com/ermaozi/get_subscribe/refs/heads/main/subscribe/v2ray.txt"
         "https://raw.githubusercontent.com/aiboboxx/v2rayfree/refs/heads/main/README.md",
         "https://raw.githubusercontent.com/Pawdroid/Free-servers/refs/heads/main/README.md",
     ]
@@ -24,10 +26,10 @@ def _find_from_github_md(nodes):
         nodes.extend(vpn_ssr.find_ssr_link(content, r"^ssr://[^\s\n]+"))
         nodes.extend(vpn_vmess.find_vmess_link(content, r"^vmess://[^\s\n]+"))
         nodes.extend(vpn_trojan.find_trojan_link(content, r"^trojan://[^\s\n]+"))
-
+    print(f"find from github text: ${len(nodes)}")
 
 def search_nodes():
     nodes = []
+    _find_from_github_text(nodes)
     _find_from_github_fq(nodes)
-    _find_from_github_md(nodes)
     return nodes
